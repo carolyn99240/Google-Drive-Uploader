f33bfdbfbaf7f93b4ecd11d8de72297a79e987f2
carolyn99240 <59831525+carolyn99240@users.noreply.github.com>
2020-05-29 20:30:56 +0530

    Update TEXT.py

--- plugins/TEXT.py
+++ plugins/TEXT.py
@@ -5,8 +5,8 @@ drive_folder_name = "GDriveUploaderBot"
 
 
 # Enter Your Mega email And Pass (Required)
-MEGA_EMAIL = "bearyan8@yandex.com"
-MEGA_PASSWORD = "bearyan8@yandex.com"
+MEGA_EMAIL = "carolyn.ballenger@my.gavilan.edu"
+MEGA_PASSWORD = "Dsp@1234"
 
 
 START = " Hi {}  \nI am Drive Uploader Bot . Please Authorise To use me .By using /auth \n\n For more info /help \n\n Third-Party Website \n Support Added /update"
@@ -32,7 +32,7 @@ HELP = """   <b>AUTHORISE BOT</b>
             
             + More On Its way:)
                 
-Bug Report @Requesting Group...
+Bug Report @Sledger Cloud
         """
 DP_DOWNLOAD = "Dropbox Link !! Downloading Started ..."
 OL_DOWNLOAD = "Openload Link !! Downloading Started ... \n Openload Links Are Extremely Slow"
